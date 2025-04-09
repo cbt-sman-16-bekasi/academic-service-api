2c51c9993177a64e4cb2cb0c1cdcfa77b70344cb
Muhammad Suryono <msuryono0@gmail.com>
2025-04-09 07:37:41 +0700

    fix password

--- src/main/helper/password.go
+++ src/main/helper/password.go
@@ -1,26 +1,21 @@
 package helper
 
 import (
-	"crypto/rand"
 	"encoding/base64"
 	"golang.org/x/crypto/argon2"
 )
 
-func generateSalt() ([]byte, error) {
-	salt := make([]byte, 16)
-	_, err := rand.Read(salt)
-	if err != nil {
-		return nil, err
-	}
-	return salt, nil
-}
+var salt = "2c+MFslHpRqi1+FOF7eepw=="
 
 func HashPasswordArgon2(password string) (string, string, error) {
-	salt, err := generateSalt()
-	if err != nil {
-		return "", "", err
-	}
+	hashed := argon2.IDKey([]byte(password), []byte(salt), 1, 64*1024, 4, 32)
+	hashBase64 := base64.StdEncoding.EncodeToString(hashed)
+	saltBase64 := base64.StdEncoding.EncodeToString([]byte(salt))
 
+	return hashBase64, saltBase64, nil
+}
+
+func HashPasswordArgon2UseSalt(password string, salt []byte) (string, string, error) {
 	hashed := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
 	hashBase64 := base64.StdEncoding.EncodeToString(hashed)
 	saltBase64 := base64.StdEncoding.EncodeToString(salt)
