51a2dd7ba77692ffa2036632600f7225dcbddd44
Muhammad Suryono <37969305+MuhammadSuryono@users.noreply.github.com>
2025-05-13 22:19:27 +0700

    fix: update user and create endpoint new user (#3)

--- src/main/model/dto/request/user_request/form.go
+++ src/main/model/dto/request/user_request/form.go
@@ -4,4 +4,5 @@ type UserUpdateRequest struct {
 	Name     string `form:"name" json:"name"`
 	Username string `form:"username" json:"username"`
 	Status   int    `form:"status" json:"status"`
+	Role     *int   `form:"role" json:"role"`
 }
