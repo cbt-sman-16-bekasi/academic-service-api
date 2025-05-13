8fe17ef2b7d9e5421eceb519cee9eee930958cf5
Muhammad Suryono <msuryono0@gmail.com>
2025-05-13 13:54:59 +0700

    fix: set column name for score question

--- src/main/model/entity/school/exam.go
+++ src/main/model/entity/school/exam.go
@@ -49,7 +49,7 @@ type Exam struct {
 	ShowResult     bool               `json:"show_result"`
 	Duration       int                `json:"duration"`
 	TypeQuestion   string             `json:"type_question" gorm:"type:varchar(50)"`
-	ScoreQuestion  int                `json:"score_question" gorm:"type:int"`
+	ScoreQuestion  int                `json:"score_question" gorm:"type:int;column:total_score;default:0"`
 	ExamMember     []ExamMember       `json:"exam_member" gorm:"foreignKey:ExamCode;references:Code"`
 	ExamQuestion   []ExamQuestion     `json:"exam_question" gorm:"foreignKey:ExamCode;references:Code"`
 	core.AuditUser
